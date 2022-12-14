2db3b9ae24d494af2e5c90c8e543e9a62d921135
Hawa656 <hawacoulibaly656@gmail.com>
2022-12-14 12:12:17 +0100

    petit modification dans index navbar et footer

---
+++ src/app/ajouter-region/ajouter-region.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajouter-region',
+  templateUrl: './ajouter-region.component.html',
+  styleUrls: ['./ajouter-region.component.scss']
+})
+export class AjouterRegionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
